7eb5cb8fd88a25733a7cf83ec8ee2385ee5dc1a0
Jason Goldstein <jason@betheshoe.com>
2017-07-30 22:49:56 -0400

    STATIC_ROOT

--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 if 'test' in sys.argv:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
