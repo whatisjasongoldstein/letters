48073beceb01eb9eb290846eef54ff6085d5ae84
Jason Goldstein <jason@betheshoe.com>
2017-09-16 13:54:00 -0400

    Manually enforce unique contraints on urls and newsletters

--- newsletters/forms.py
+++ newsletters/forms.py
@@ -7,11 +7,20 @@ class SourceForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(SourceForm, self).__init__(*args, **kwargs)
-
         # You can't delete things that aren't
         # yet saved.
         if not self.instance.id:
             self.fields["delete"].widget = forms.HiddenInput()
+
+    def clean_url(self):
+        """
+        Unique together isn't being enforced
+        at the form level. Force it.
+        """
+        url = self.cleaned_data['url']
+        if Source.objects.filter(url=url).exclude(id=self.instance.id).exists():
+            raise forms.ValidationError("You already have this feed!")
+        return url
 
     class Meta:
         model = Source
