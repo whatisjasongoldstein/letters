f1b64c4f225324054a6d27a91dcf8158da61b8ca
Jason Goldstein <jason@betheshoe.com>
2018-09-03 15:43:25 -0400

    Keep this

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', dashboard, name="dashboard"),
     path('preview/', preview, name="preview"),
     path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('accounts/logout/', auth_views.logout_then_login, name="logout"),
     path('admin/', admin.site.urls),
 ]
